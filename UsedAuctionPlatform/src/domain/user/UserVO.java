8012d80ce6789bc7a74d7f2951a40e5cc4339aa4
KOSTA <KOSTA@DESKTOP-KKPIIQ0>
2024-05-14 15:24:20 +0900

    전성훈 userVO 멤버변수

--- UsedAuctionPlatform/src/domain/user/UserVO.java
+++ UsedAuctionPlatform/src/domain/user/UserVO.java
@@ -1,5 +1,19 @@
 package domain.user;
 
 public class UserVO {
-
+	private String user_id;
+	private String password;
+	private String name;
+	private String nickName;
+	private String phoneNumber;
+	private String email;
+	private String address;
+	private int point;
+	private double rate;
+	private int rateCount;
+	private String profileImg;
+	private String userType;
+	private int reportCount;
+	
+	
 }
