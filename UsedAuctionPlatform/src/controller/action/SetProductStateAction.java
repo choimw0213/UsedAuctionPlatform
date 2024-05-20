4a30f0a3cab3eb00bf2af60557c59e13a6e34914
cmw <cmw@DESKTOP-T46B2KL>
2024-05-20 10:55:41 +0900

    최민우 #1. 깡통 Action 파일 생성

--- UsedAuctionPlatform/src/controller/action/SetProductStateAction.java
+++ UsedAuctionPlatform/src/controller/action/SetProductStateAction.java
@@ -8,20 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import controller.Action;
 import controller.URLModel;
 
-public class LoginAction implements Action {
+public class SetProductStateAction implements Action {
 
 	@Override
 	public URLModel execute(HttpServletRequest request) throws ServletException, IOException {
-
-		String userId = request.getParameter("userId");
-		String userPw = request.getParameter("userPw");
-		
-		System.out.println(userId + " " + userPw);
-		
-		//loginService.login();
-		
-//		return null;
-		return new URLModel("mainUI.jsp", true);
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
